149d4b3fd3b37e4183a58e9aba5f7e8ec81654d3
Bazyl Reinstein <bazyl.reinstein@bjss.com>
2018-04-04 17:39:20 +0200

    creates project

---
+++ Proficiency Exercise/Proficiency Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proficiency Exercise
+//
+//  Created by Bazyl Reinstein on 04/04/2018.
+//  Copyright © 2018 Bazyl Reinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
